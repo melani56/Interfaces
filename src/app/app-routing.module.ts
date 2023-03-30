b456ed136b2a7b94a32cc17650ce4492559b6553
melani56 <melaniarce254@gmail.com>
2023-03-30 17:03:02 -0500

    primer comit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,14 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import {SeguimientoComponent} from "./vistas/seguimiento/seguimiento.component";
+import{DerivacionComponent} from "./derivacion/derivacion.component";
+import{FCierreComponent} from "./f-cierre/f-cierre.component";
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:'seguimiento', component:SeguimientoComponent},
+  {path:'derivacion', component:DerivacionComponent},
+  {path:'cierre', component:FCierreComponent},
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,17 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { SeguimientoComponent } from './vistas/seguimiento/seguimiento.component';
+import { DerivacionComponent } from './derivacion/derivacion.component';
+import { FCierreComponent } from './f-cierre/f-cierre.component';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SeguimientoComponent,
+    DerivacionComponent,
+    FCierreComponent,
   ],
   imports: [
     BrowserModule,
